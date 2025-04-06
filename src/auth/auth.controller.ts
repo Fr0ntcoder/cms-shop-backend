b9774df2b80a1a0dfee31e2d93118056cceb659b
Fr0ntcoder <jambox228@gmail.com>
2025-04-06 22:15:14 +0800

    auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,118 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { Request, Response } from 'express'
+import { AuthDto } from 'src/auth/dto/auth.dto'
+import { AuthService } from './auth.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refereshToken, ...response } = await this.authService.login(dto)
+
+    this.authService.addRefreshTokenToResponse(res, refereshToken)
+
+    return response
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refereshToken, ...response } = await this.authService.register(dto)
+
+    this.authService.addRefreshTokenToResponse(res, refereshToken)
+
+    return response
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getNewTokens(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME]
+
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenFromResponse(res)
+      throw new UnauthorizedException('Refresh token is missing')
+    }
+    const { refereshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    )
+
+    this.authService.addRefreshTokenToResponse(res, refereshToken)
+
+    return response
+  }
+
+  @HttpCode(200)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromResponse(res)
+
+    return true
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthCallback(
+    @Req() req: any,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refereshToken, ...response } =
+      await this.authService.validateOAuthLogin(req)
+
+    this.authService.addRefreshTokenToResponse(res, refereshToken)
+
+    return res.redirect(
+      `${process.env.CLIENT_URL}/dashboard?accessToken=${response.accessToken}`,
+    )
+  }
+
+  @Get('yandex')
+  @UseGuards(AuthGuard('yandex'))
+  async yandexAuth(@Req() _req) {}
+
+  @Get('yandex/callback')
+  @UseGuards(AuthGuard('yandex'))
+  async yandexAuthCallback(
+    @Req() req: any,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refereshToken, ...response } =
+      await this.authService.validateOAuthLogin(req)
+
+    this.authService.addRefreshTokenToResponse(res, refereshToken)
+
+    return res.redirect(
+      `${process.env.CLIENT_URL}/dashboard?accessToken=${response.accessToken}`,
+    )
+  }
+}
