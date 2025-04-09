bceaa939e9ed827863de595ade4f04ae7856771c
Fr0ntcoder <jambox228@gmail.com>
2025-04-09 21:33:45 +0800

    order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorator/auth.decorator'
+import { OrderDto } from 'src/order/dto/order.dto'
+import { CurrentUser } from 'src/user/decorators/user.decorator'
+import { PaymentStatusDto } from './dto/payment.dto'
+import { OrderService } from './order.service'
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('place')
+  @Auth()
+  async checkout(@Body() dto: OrderDto, @CurrentUser('id') userid: string) {
+    return this.orderService.createPayment(dto, userid)
+  }
+
+  @HttpCode(200)
+  @Post('status')
+  async updateStatus(@Body() dto: PaymentStatusDto) {
+    return this.orderService.updateStatus(dto)
+  }
 }
